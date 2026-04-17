340129fd3547ec28933aa4311bba458218c84771
BishwoSubedi <bishwosubedu@example.com>
2026-04-17 01:42:33 +0100

    eight_commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,34 +1,34 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: true,
-  }
-);
-
-
 // const { Sequelize } = require("sequelize");
 // require("dotenv").config();
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   logging: false, // clean logs in production
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: true,
+//   }
+// );
 
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
 
-// module.exports = sequelize;
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false, // clean logs in production
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
 
 module.exports = sequelize;
